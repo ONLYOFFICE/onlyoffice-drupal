034b2e7752ccecc78495e60982791e4d7c22938e
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-03-19 00:19:26 +0500

    create a form for filtering onlyoffice forms table

---
+++ modules/onlyoffice-form/src/Form/OnlyofficeFormFilterForm.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Drupal\onlyoffice_form\Form;
+
+/**
+ * Copyright (c) Ascensio System SIA 2025.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides the ONLYOFFICE form filter form.
+ */
+class OnlyofficeFormFilterForm extends FormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'onlyoffice_form_filter_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $instance = parent::create($container);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $search = NULL, $state = NULL, array $state_options = []) {
+    $form['#attributes'] = ['class' => ['onlyoffice-form-filter-form']];
+    $form['filter'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Filter forms'),
+      '#open' => TRUE,
+      '#attributes' => ['class' => ['container-inline']],
+    ];
+    $form['filter']['search'] = [
+      '#type' => 'search',
+      '#title' => $this->t('Keyword'),
+      '#title_display' => 'invisible',
+      '#placeholder' => $this->t('Filter by keyword'),
+      '#size' => 45,
+      '#default_value' => $search,
+    ];
+    $form['filter']['state'] = [
+      '#type' => 'select',
+      '#title' => $this->t('State'),
+      '#title_display' => 'invisible',
+      '#options' => $state_options,
+      '#default_value' => $state,
+    ];
+    $form['filter']['submit'] = [
+      '#type' => 'submit',
+      '#button_type' => 'primary',
+      '#value' => $this->t('Filter'),
+    ];
+    if (!empty($search) || !empty($state)) {
+      $form['filter']['reset'] = [
+        '#type' => 'submit',
+        '#submit' => ['::resetForm'],
+        '#value' => $this->t('Reset'),
+      ];
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $search = $form_state->getValue('search') ?? '';
+    $state = $form_state->getValue('state') ?? '';
+    $query = [
+      'search' => trim($search),
+      'state' => trim($state),
+    ];
+    $form_state->setRedirect($this->getRouteMatch()->getRouteName(), $this->getRouteMatch()->getRawParameters()->all(), [
+      'query' => $query,
+    ]);
+  }
+
+  /**
+   * Resets the filter selection.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function resetForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect($this->getRouteMatch()->getRouteName(), $this->getRouteMatch()->getRawParameters()->all());
+  }
+
+}
