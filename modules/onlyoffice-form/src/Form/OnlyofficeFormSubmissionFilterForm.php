5a5bc2b4457c8b3deb0bad1577dd03454a3bb24c
Serik Ibragimov <serik.kerimbayevich@onlyoffice.com>
2025-03-19 18:54:19 +0500

    create a filter for submissions list

---
+++ modules/onlyoffice-form/src/Form/OnlyofficeFormSubmissionFilterForm.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\onlyoffice_form\Form;
+
+/**
+ * Copyright (c) Ascensio System SIA 2025.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a form for filtering ONLYOFFICE form submissions.
+ */
+class OnlyofficeFormSubmissionFilterForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'onlyoffice_form_submission_filter_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $keys = '') {
+    $form['#attributes'] = ['class' => ['onlyoffice-form-filter-form']];
+    $form['filter'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Filter submissions'),
+      '#open' => TRUE,
+      '#attributes' => ['class' => ['container-inline']],
+    ];
+    
+    $form['filter']['search'] = [
+      '#type' => 'search',
+      '#title' => $this->t('Keyword'),
+      '#title_display' => 'invisible',
+      '#placeholder' => $this->t('Filter by keyword'),
+      '#size' => 45,
+      '#default_value' => $keys,
+    ];
+    
+    $form['filter']['submit'] = [
+      '#type' => 'submit',
+      '#button_type' => 'primary',
+      '#value' => $this->t('Filter'),
+    ];
+    
+    if (!empty($keys)) {
+      $form['filter']['reset'] = [
+        '#type' => 'submit',
+        '#submit' => ['::resetForm'],
+        '#value' => $this->t('Reset'),
+      ];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $search = $form_state->getValue('search') ?? '';
+    $query = [
+      'search' => trim($search),
+    ];
+    $form_state->setRedirect($this->getRouteMatch()->getRouteName(), $this->getRouteMatch()->getRawParameters()->all(), [
+      'query' => $query,
+    ]);
+  }
+
+  /**
+   * Resets the filter selection.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function resetForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect($this->getRouteMatch()->getRouteName(), $this->getRouteMatch()->getRawParameters()->all());
+  }
+
+}
